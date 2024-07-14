5f01893975ba63a5f86134b0c0a5dcf88b3828b1
Dihad Hasan <130214955+Dihad@users.noreply.github.com>
2024-07-14 17:59:01 +0600

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,83 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Name a great Idiot?",
+    answer: "DIHAD",
+    options: [
+      "ADIB",
+      "DIHAD",
+      "HIADI",
+      "ARJU"
+    ]
+  },
+    {
+    numb: 2,
+    question: "when the Idiot was born?",
+    answer: "2010",
+    options: [
+      "2009",
+      "2010",
+      "2011",
+      "2012"
+    ]
+  },
+    {
+    numb: 3,
+    question: "why he is a Idiot?",
+    answer: "he makes idiocratic stuff",
+    options: [
+      "he makes idiocratic stuff",
+      "he call himself an idiot",
+      "he makes useless stuff",
+      "he makes usefull stuff"
+    ]
+  },
+    {
+    numb: 4,
+    question: "idiot's favorite hobby?",
+    answer: "coding",
+    options: [
+      "reading",
+      "coding",
+      "making joke",
+      "doesn't have a hobby"
+    ]
+  },
+  {
+    numb: 5,
+    question: "how many siblings do I have?",
+    answer: "sister",
+    options: [
+      "none",
+      "sister",
+      "brother",
+      "sister and brother"
+    ]
+  },
+  {
+    numb: 6,
+    question: "what i hate the most?",
+    answer: "education system",
+    options: [
+      "music",
+      "society's criticism",
+      "traditional teachers",
+      "education system"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
